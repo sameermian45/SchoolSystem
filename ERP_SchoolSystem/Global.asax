db6ffdc1ce2bb6985fe246581ace681762ecfc71
HP <HP@DESKTOP-HPIAV9J>
2019-04-28 19:29:14 -0700

    Import changes

---
+++ ERP_SchoolSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ERP_SchoolSystem.MvcApplication" Language="C#" %>
---
+++ ERP_SchoolSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ERP_SchoolSystem.Startup))]
+namespace ERP_SchoolSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
