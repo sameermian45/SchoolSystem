22f2bcbfe031210c3b7fe53b7e6889ec2eb98ce4
HP <HP@DESKTOP-HPIAV9J>
2019-05-01 05:05:37 -0700

    Jquery add

---
+++ ERP_SchoolSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERP_SchoolSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // Employe
+        public ActionResult CreateEmploye()
+        {
+                      
+            return View();
+        }
+        // List All Employee
+
+    }
+}
